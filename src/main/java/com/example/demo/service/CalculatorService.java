7998de7a2cbf4a590745e63cce735fd5bf3de6dc
tgshivkumar <tgshivkumar94@gmail.com>
2025-06-16 15:17:19 +0530

    test commit 2

--- src/main/java/com/example/demo/service/CalculatorService.java
+++ src/main/java/com/example/demo/service/CalculatorService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CalculatorService {
-    public int add(int a, int b)        { return a + b; }
+    public int add(int a, int b)        { return b + a; }
     public int subtract(int a, int b)   { return a - b; }
     public int multiply(int a, int b)   { return a * b; }
     public int divide(int a, int b) {
