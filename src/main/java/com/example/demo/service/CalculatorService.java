db3063743b499731ff3cb56394ea432fe9bab9f0
tgshivkumar <tgshivkumar94@gmail.com>
2025-06-16 16:50:24 +0530

    check commit

--- src/main/java/com/example/demo/service/CalculatorService.java
+++ src/main/java/com/example/demo/service/CalculatorService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class CalculatorService {
     public int add(int a, int b)        { return b + a; }
-    public int subtract(int a, int b)   { return a - b; }
+    public int subtract(int a, int b)   { return - b + a; }
     public int multiply(int a, int b)   { return a * b; }
     public int divide(int a, int b) {
         if (b == 0) throw new IllegalArgumentException("Cannot divide by zero");
